0d5c46e91adf98f3a6b21b85828104e4ecd7f2a7
joelyakubu269 <joelyakubu269@gmail.com>
2026-07-02 08:04:47 +0100

    first commit

--- web/generate.go
+++ web/generate.go
@@ -1,13 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func Generate(s string, banner map[rune][]string) (string, error) {
 
 }
-func Render(s string, banner map[rune][]string) {
+func Render(s string, banner map[rune][]string) (string, error) {
+	var res strings.Builder
 
+	for i := 0; i < 8; i++ {
+		for _, ch := range s {
+			val, ok := banner[ch]
+			if ok {
+				res.WriteString(val[i])
+			} else {
+				return "", fmt.Errorf("character not found")
+			}
+
+		}
+		res.WriteString("\n")
+	}
+	return res.String(), nil
 }
 func validate(s string) error {
 	for _, r := range s {
-
+		if r == '\r' || r == '\n' {
+			continue
+		}
+		if r < 32 || r > 126 {
+			return fmt.Errorf("invalid character")
+		}
 	}
+	return nil
 }
