0f9f12bfb6eafebbc58fb48c61020434d077b7d3
joelyakubu269 <joelyakubu269@gmail.com>
2026-07-02 07:36:40 +0100

    first commit

--- web/banner.go
+++ web/banner.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"strings"
 )
@@ -12,4 +13,14 @@ func LoadBnner(filename string) (map[rune][]string, error) {
 		return nil, err
 	}
 	lines := strings.Split(string(data), "\n")
+	if len(lines) < 855 {
+		return nil, fmt.Errorf("invalid banner format")
+	}
+	m := make(map[rune][]string)
+	for i := 32; i <= 126; i++ {
+		start := ((i - 32) * 9) + 1
+		end := start + 8
+		m[rune(i)] = lines[start:end]
+	}
+	return m, nil
 }
