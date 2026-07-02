273bfd9078fcbbf2199c72a4469b19feb5216294
joelyakubu269 <joelyakubu269@gmail.com>
2026-07-02 07:40:04 +0100

    first commit

---
+++ web/generate.go
@@ -0,0 +1,13 @@
+package main
+
+func Generate(s string, banner map[rune][]string) (string, error) {
+
+}
+func Render(s string, banner map[rune][]string) {
+
+}
+func validate(s string) error {
+	for _, r := range s {
+
+	}
+}
