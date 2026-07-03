d610b802359cc5816d49bfe33993903e1523b438
joelyakubu269 <joelyakubu269@gmail.com>
2026-07-03 14:31:57 +0100

    im very commited

--- web/main.go
+++ web/main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", handleHome)
-	http.HandleFunc("/switch-handler", SwitchHandler)
+	http.HandleFunc("/ascii-art-switch", SwitchHandler)
 	http.HandleFunc("/ascii-art", asciiHandler)
 	http.ListenAndServe(":8080", nil)
 }
