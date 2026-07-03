10c0615fef3da319392504d807505836a866cd01
joelyakubu269 <joelyakubu269@gmail.com>
2026-07-03 13:18:20 +0100

    first commit

--- web/handler.go
+++ web/handler.go
@@ -36,9 +36,34 @@ func asciiHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	text := r.FormValue("text")
 	banner := r.FormValue("banner")
+	if text == "" || banner == "" {
+		http.Error(w, "field cannot be empty", http.StatusBadRequest)
+		return
+	}
 	m, err := LoadBnner(banner)
 	if err != nil {
 		http.Error(w, "banner not found", http.StatusNotFound)
+		return
+	}
+	err = validate(text)
+	if err != nil {
+		http.Error(w, "bad reqquest", http.StatusBadRequest)
+		return
+	}
+	result, err := Generate(text, m)
+	if err != nil {
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+	templ.Execute(w, PageData{
+		Text:   text,
+		Banner: banner,
+		Result: result,
+	})
+}
+func SwitchHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/switch-handler" {
+		http.Error(w, "status not found", http.StatusNotFound)
 		return
 	}
 }
